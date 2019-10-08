89f1ea091f86ee49aaceea59b152e4b1519bf071
Vinícius Meng <vinimeng@hotmail.com>
2019-10-08 12:45:33 -0300

    SubMenus :zap:

---
+++ Inserts_de_SubMenus.sql
@@ -0,0 +1,153 @@
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (1, 3, 'Digitar Laudo', '/Editor/digitar', null, 'S', 1, 'digitar', 'pencil', 'btn-primary');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (2, 3, 'Imprimir Laudo', '/Editor/imprimir', null, 'S', 2, 'imprimir', 'print', 'btn-info');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (3, 3, 'Visualizar Laudo', '/Editor/visualizar', null, 'S', 3, 'visualizar', 'book', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (4, 3, 'Situação', '#', null, 'S', 4, '#', 'tag', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (5, 3, 'Prioridade', '/FluxoDigitacao/prioridadeStatus', null, 'S', 7, 'prioridadeStatus', 'exclamation-triangle', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (6, 3, 'Pendência', '/Fluxo/pendencia', null, 'S', 8, 'pendencia', 'asterisk', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (7, 3, 'Rastreamento', '/Editor/rastreamento', null, 'S', 10, 'rastreamento', 'crosshairs', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (8, 3, 'Imprimir Etiqueta', '/FluxoDigitacao/buscaEtiqueta', null, 'S', 14, 'buscaEtiqueta', 'barcode', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (9, 3, 'Liberado', '/FluxoDigitacao/liberar', 4, 'S', 1, 'liberar', 'check', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (10, 3, 'Fechamento', '/FluxoDigitacao/envelopar', 4, 'S', 2, 'envelopar', 'envelope', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (11, 3, 'Disponível', '/FluxoDigitacao/pronto', 4, 'S', 3, 'pronto', 'cloud-upload', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (12, 3, 'Cancelar Liberado', '/FluxoDigitacao/cancelarLiberar', 4, 'S', 5, 'cancelarLiberar', 'check sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (13, 3, 'Cancelar Fechamento', '/FluxoDigitacao/cancelarEnvelopar', 4, 'S', 6, 'cancelarEnvelopar', 'envelope sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (14, 3, 'Cancelar Disponível', '/FluxoDigitacao/cancelarPronto', 4, 'S', 7, 'cancelarPronto', 'cloud-upload sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (15, 4, 'Digitar Laudo', '/Editor/digitar', null, 'S', 2, 'digitar', 'pencil', 'btn-primary');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (16, 4, 'Revisar Laudo', '/Editor/revisar', null, 'S', 3, 'revisar', 'eye', 'btn-warning');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (17, 4, 'Imprimir Laudo', '/Editor/imprimir', null, 'S', 4, 'imprimir', 'print', 'btn-info');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (18, 4, 'Visualizar Laudo', '/Editor/visualizar', null, 'S', 5, 'visualizar', 'book', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (19, 4, 'Ditar Laudo', '/Editor/ditar', null, 'S', 1, 'ditar', 'microphone', 'btn-danger');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (20, 4, 'Situação', '#', null, 'S', 10, '#', 'tag', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (22, 4, 'Cancelar Revisado', '/FluxoMedico/cancelarRevisar', 20, 'S', 2, 'cancelarRevisar', 'eye sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (23, 4, 'Cancelar Assinado', '/FluxoMedico/cancelarAssinar', 20, 'S', 3, 'cancelarAssinar', 'check sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (24, 4, 'Assinar Laudo', '/FluxoMedico/assinar', null, 'N', null, 'assinar', 'check', 'btn-purple');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (25, 3, 'Informações', '/FluxoDigitacao/info', null, 'N', 13, 'info', '', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (26, 29, 'Excluir Laudo', '/Editor/excluirLaudo', null, 'N', null, 'excluir', '', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (27, 28, 'Excluir Ditado', '/Editor/excluirDitado', null, 'N', null, 'excluirDitado', '', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (28, 29, 'Salvar', '/Editor/salvar', null, 'N', null, 'salvar', 'save', 'btn-success');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (29, 29, 'Inserir Laudo Padrão', '/Editor/textoPadrao', null, 'N', 99, 'textoPadrao', 'star', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (30, 4, 'Pendência - Fluxo', '/Fluxo/pendencia', null, 'S', 9, 'pendencia', 'asterisk', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (31, 3, 'Importar Laudo', '/Fluxo/importaLaudoPDF', null, 'S', 16, 'importaLaudoPDF', 'upload', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (32, 4, 'Importar Laudo', '/Fluxo/importaLaudoPDF', null, 'S', 11, 'importaLaudoPDF', 'upload', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (33, 49, 'Imprimir Laudo', '/Editor/imprimir', null, 'S', 3, 'imprimir', 'print', 'btn-info');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (34, 4, 'Visualizar Imagem', '/Pacs/estudo', null, 'S', 12, 'estudo', 'desktop', 'btn-black');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (35, 3, 'Duplicar Laudo', '/Fluxo/duplicar', null, 'S', 5, 'duplicar', 'clone', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (36, 4, 'Duplicar Laudo', '/Fluxo/duplicar', null, 'S', 6, 'duplicar', 'clone', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (37, 49, 'Visualizar Imagem', '/Pacs/estudo', null, 'S', 4, 'estudo', 'desktop', 'btn-black');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (38, 3, 'Atribuir Laudo', '/FluxoDigitacao/atribuir', null, 'S', 5, 'atribuir', 'share', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (39, 51, 'Visualizar Imagem', '/Pacs/estudo', null, 'N', 1, 'estudo', 'desktop', 'btn-black');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (40, 3, 'Fechamento Exames', '/FluxoDigitacao/fechamento', null, 'N', 17, 'fechamento', 'envelope', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (41, 3, 'Liberar Web', '/FluxoDigitacao/liberarWeb', null, 'N', 18, 'liberarWeb', 'cloud-upload', 'btn-warning');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (42, 29, 'Laudo OIT', '/LaudoOIT/laudoOIT', null, 'N', 100, 'laudoIOT', 'cube', 'btn-primary');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (43, 3, 'Cancelar Entregue', '/FluxoDigitacao/cancelarEntregue', 4, 'S', 8, 'cancelarEntregue', 'paper-plane-o sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (44, 3, 'Entregue', '/FluxoDigitacao/entregue', 4, 'S', 4, 'entregue', 'paper-plane-o', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (45, 4, 'Cancelar Ditado', '/FluxoMedico/cancelarDitar', 20, 'S', 1, 'cancelarDitar', 'microphone sys-icon-red', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (46, 4, 'Transferir Realizante', '/FluxoMedico/transferirRealizante', null, 'S', 7, 'transferirRealizante', 'long-arrow-right', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (47, 3, 'Importar Áudio', '/FluxoDigitacao/importarAudio', null, 'S', 15, 'importarAudio', 'music', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (48, 3, 'Alterar Data Entrega', '/FluxoDigitacao/alterarDataEntrega', null, 'S', 12, 'alterarDataEntrega', 'calendar', '');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (49, 97, 'Transferir Realizante', '/FluxoControleLaudo/transferirRealizante', null, 'S', 2, 'transferirRealizante', 'long-arrow-right', 'btn-primary');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (50, 3, 'Pendência - Atendimento', '/Fluxo/pendenciaAtdo', null, 'S', 9, 'pendenciaAtdo', 'dot-circle-o', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (51, 97, 'Histórico de Realizante', '/FluxoControleLaudo/historicoRealizante', null, 'S', 1, 'historicoRealizante', 'history', 'btn-default');
+
+insert into web_subcontrole (CODSUBCONTROLE, CODCONTROLE, LABEL, CONTROLE, CODSUBCONTROLEPAI, VISIVEL, INDICE, ACAO, ICONE, CSS)
+values (52, 4, 'Transferência PADI', '/FluxoMedico/transferirPADI', null, 'S', 8, 'transferirPADI', 'gavel', 'btn-default');
+
